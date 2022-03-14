94cf0ef9fc4280737d6ea5eafa58df1c5e3626b1
Lộc Lê <lexuanlocsplt2017@gmail.com>
2022-03-14 13:17:53 +0700

    single responsibility principle and open close principle

---
+++ OpenClosePrinciple/Before/EmployeeReport.cs
@@ -0,0 +1,8 @@
+public class EmployeeReport
+{
+    public int Id { get; set; }
+    public int WorkingHours { get; set; }
+    public double HourlyRate { get; set; }
+    public string Name { get; set; }
+    public string Level { get; set; }
+}
